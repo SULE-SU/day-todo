716d6740282dd2e256cf83c326ac01a0dae8b9c7
SULE <sule1@oocl.com>
2025-09-14 13:06:31 +0800

    feat : update todo list UI-app.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import {TodoContext} from "./contexts/TodoContext";
 
 
 export const initState = [
-    {id: 1, text: "the first todo", done: false},
-    {id: 2, text: "the second todo", done: false},
+    {id: 1, text: "这是我需要做的第一件事", done: false},
+    {id: 2, text: "这是第二件我需要做的事情", done: false},
+    {id: 3, text: "我已经完成了这个任务", done: true},
 ];
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
     return (
-        <div>
-            <TodoContext value={{state, dispatch}}>
+        <div className="app">
+            <TodoContext.Provider value={{state, dispatch}}>
+                <h1>待办事项列表</h1>
                 <TodoGroup/>
-            </TodoContext>
+            </TodoContext.Provider>
         </div>
     );
 }
