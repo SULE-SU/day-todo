2be98942d742871c7b3f491ca8ff9b7f11873cc4
SULE <sule1@oocl.com>
2025-09-15 10:48:03 +0800

    refactor : add 404 ErrorPage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import {todoReducer} from "./reducer/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
 import {TodoList} from "./components/TodoList";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider, useRouteError} from "react-router";
 
 function DefaultLayout() {
     return <div>
@@ -20,10 +20,21 @@ function DefaultLayout() {
     </div>
 }
 
+function ErrorPage() {
+    const error = useRouteError();
+    return <div>
+        { error.status === 404
+            ? <h1>404 not found</h1>
+            : <div>{JSON.stringify(error)}</div>
+        }
+    </div>
+}
+
 const routes= createBrowserRouter([
     {
         path: "/",
         element: <DefaultLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
