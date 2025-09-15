a80377c5bb8eaa68040131182f61a1c2ba176fac
SULE <sule1@oocl.com>
2025-09-15 11:09:05 +0800

    refactor: extra routes and layouts components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,9 @@
-import {useContext, useReducer} from "react";
+import {useReducer} from "react";
 import "./App.css"
 import {todoReducer} from "./reducer/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider, useParams} from "react-router";
-import {ErrorPage} from "./pages/ErrorPage";
-import {HomePage} from "./pages/HomePage";
-import {TodoItem} from "./components/TodoItem";
-
-function DefaultLayout() {
-    return <div>
-        <header>
-            <nav>
-                <ul>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/todos/:id">Detail</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <Outlet />
-        </main>
-    </div>
-}
-
-function TodoDetailPage() {
-    const {id} = useParams();
-    const {state} = useContext(TodoContext);
-    const todo = state.filter(todo => todo.id === parseInt(id))
-
-    if (todo.length === 0) {
-        return <div>
-            NOT FOUND TODO
-        </div>
-    }
-    return <div>
-        <TodoItem todo={todo[0]} index={id} />
-    </div>
-}
-
-const routes= createBrowserRouter([
-    {
-        path: "/",
-        element: <DefaultLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "/",
-                element: <HomePage />,
-            },
-            {
-                path: "/todos/:id",
-                element: <TodoDetailPage />,
-            }
-        ]
-    }
-]);
+import {RouterProvider} from "react-router";
+import {routes} from "./Routes/Routes";
 
 export const initState = [
     {id: 1, text: "这是我需要做的第一件事", done: false},
---
+++ src/pages/TodoDetailPage.js
@@ -0,0 +1,19 @@
+import {useParams} from "react-router";
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import {TodoItem} from "../components/TodoItem";
+
+export function TodoDetailPage() {
+    const {id} = useParams();
+    const {state} = useContext(TodoContext);
+    const todo = state.filter(todo => todo.id === parseInt(id))
+
+    if (todo.length === 0) {
+        return <div>
+            NOT FOUND TODO
+        </div>
+    }
+    return <div>
+        <TodoItem todo={todo[0]} index={id}/>
+    </div>
+}
