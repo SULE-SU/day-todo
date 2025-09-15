0f9918fca92f505357891180c9a6beedda92d2e8
SULE <sule1@oocl.com>
2025-09-15 09:22:43 +0800

    refactor : delete usuless code

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -28,33 +28,3 @@ export function TodoItem(props) {
         <button className="delete-btn" onClick={deleteTodo}>X</button>
     </div>;
 }
-
-
-export function todoReducer(state, action) {
-    switch (action.type) {
-        case "TOGGLE_TODO":
-            const newState = [...state];
-            const id = action.payload.id;
-            return newState.map((value) => {
-                if (value.id === id) {
-                    return {
-                        id,
-                        text: value.text,
-                        done: !value.done
-                    };
-                }
-                return value
-            })
-        case "ADD_TODO":
-            const newId = state.length > 0 ? Math.max(...state.map(todo => todo.id)) + 1 : 1;
-            return [...state, {
-                id: newId,
-                text: action.payload.text,
-                done: false
-            }];
-        case "DELETE_TODO":
-            return state.filter(todo => todo.id !== action.payload.id);
-        default:
-            return state;
-    }
-}
